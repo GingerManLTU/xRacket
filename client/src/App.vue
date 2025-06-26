0412d343e4364b64ff69f99497ccc638086a3935
Patrikas Karpickas <patrikas.karpickas@mif.stud.vu.lt>
2025-06-27 01:43:20 +0300

    chore: setup Vue3

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import SearchBar from './components/SearchBar.vue'
+import ResultsLog from './components/ResultsLog.vue'
+
+const weather = ref()
+
+const Results = (res) => {
+  weather.value = res
+}
+</script>
+
+<template>
+  <main>
+    <SearchBar @results="Results" />
+    <ResultsLog :weather="weather" />
+  </main>
+</template>
+
+<style scoped></style>
